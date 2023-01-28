ee7beef663ba2848dfc52ae7743a52639b930f77
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-01-28 21:00:20 +0200

    simplelightbox

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import SimpleLightbox from "simplelightbox";
+import 'react-toastify/dist/ReactToastify.css';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+export default class App extends React.Component {
+  state = {
+
+  }
+
+  render () {
+    return (
+      <>
+      <ToastContainer  autoClose={3000}/>
+      </>
+    )
+  }
+
+}
 
 
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
